afb9611282bcd42bd74d94728c1d20fa2c30190d
Tristan_Day <tmd526@gmail.com>
2023-12-07 22:39:48 -0600

    got image recognition and will send email to my email address

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
+import glob
+import os
 import cv2
 import time
-
 from send_email import send_email
+from threading import Thread
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
 first_frame = None
 status_list = []
+count = 1
+
+
+def clean_folder():
+    print("clean folder function started")
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
+    print("clean folder function ended")
+
+
 while True:
     status = 0
     check, frame = video.read()
@@ -29,15 +42,26 @@ while True:
         if cv2.contourArea(contour) < 5000:
             continue
         x, y, w, h = cv2.boundingRect(contour)
-        rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0 , 255, 0), 3)
+        rectangle = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count += 1
+            all_images = glob.glob("images/*.png")
+            image_with_object = all_images[(int(len(all_images) / 2))]
 
     status_list.append(status)
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        email_thread = Thread(target=send_email, args=(image_with_object, ))
+        email_thread.daemon = True
+        clean_thread = Thread(target=clean_folder)
+        clean_thread.daemon = True
+
+        email_thread.start()
+        clean_thread.start()
+
     cv2.imshow("My video", frame)
 
     key = cv2.waitKey(1)
